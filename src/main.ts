cd6aa8f7d415404a87d55149faa4d618ada76398
oppong-coffie <teama@teamalfy.com>
2024-12-06 13:39:23 +0000

    cors error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use CORS middleware
+  app.use(cors({
+    origin: '*', // Allow all origins
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
