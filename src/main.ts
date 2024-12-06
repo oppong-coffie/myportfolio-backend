82720899055c89786a8cc85b623d5da94e5359d9
oppong-coffie <teama@teamalfy.com>
2024-12-06 15:43:18 +0000

    sms done

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Use CORS middleware
-  app.use(cors({
+  // Enable CORS to allow requests from any origin
+  app.enableCors({
     origin: '*', // Allow all origins
-  }));
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allow necessary HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Allow required headers
+  });
 
   await app.listen(3000);
 }
